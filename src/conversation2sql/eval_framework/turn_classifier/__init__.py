467637edd4caaa3c491194220c08647ae1372a83
simone papicchio <simopapi@hotmail.com>
2026-05-07 20:22:24 +0000

    feat: add TurnClassifier orchestrating Level 2 + Level 1 LLM judge

--- src/conversation2sql/eval_framework/turn_classifier/__init__.py
+++ src/conversation2sql/eval_framework/turn_classifier/__init__.py
@@ -3,5 +3,6 @@ from conversation2sql.eval_framework.turn_classifier.schemas import (
     Level1Classification,
     TurnClassification,
 )
+from conversation2sql.eval_framework.turn_classifier.classifier import TurnClassifier
 
-__all__ = ["Level1Classification", "TurnClassification"]
+__all__ = ["Level1Classification", "TurnClassification", "TurnClassifier"]
